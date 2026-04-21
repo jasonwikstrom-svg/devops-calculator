c820561c0154eeba4d900bf6c95d18526773107b
jasonwikstrom-svg <jasonwikstrom30@gmail.com>
2026-04-21 10:21:14 +0200

    Finalize calculator UI

--- src/main.js
+++ src/main.js
@@ -2,19 +2,23 @@ import './style.css'
 import { add, subtract, multiply, divide } from '../math.js'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Min DevOps-app</h1>
+  <div class="container">
+    <h1>Min DevOps-app</h1>
 
-  <input id="num1" type="number" placeholder="Tal 1" />
-  <input id="num2" type="number" placeholder="Tal 2" />
+    <input id="num1" type="number" placeholder="Tal 1" />
+    <input id="num2" type="number" placeholder="Tal 2" />
 
-  <div>
-    <button id="add">+</button>
-    <button id="sub">-</button>
-    <button id="mul">*</button>
-    <button id="div">/</button>
+    <div>
+      <button id="add">+</button>
+      <button id="sub">-</button>
+      <button id="mul">*</button>
+      <button id="div">/</button>
+    </div>
+
+    <div class="card">
+      <p id="result"></p>
+    </div>
   </div>
-
-  <p id="result"></p>
 `
 
 const getNumbers = () => {
